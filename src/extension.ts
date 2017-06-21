e3657a5cd14d0a03a20038ff360dab28a4de499e
Roman Peshkov <peshkovroman@gmail.com>
2017-06-22 00:19:00 +0200

    Added view actions, added output of go test, some refactor

--- src/extension.ts
+++ src/extension.ts
@@ -5,36 +5,55 @@ import * as vscode from 'vscode';
 import * as child from 'child_process';
 
 import { GoTestsProvider } from './go-tests-provider';
+import { TreeNode, TreeNodeType } from './model/tree-node';
 
 export function activate(context: vscode.ExtensionContext) {
     const rootPath = vscode.workspace.rootPath;
 
-    var goTestsProvider = new GoTestsProvider(rootPath);
+    const goTestsProvider = new GoTestsProvider(rootPath);
     vscode.window.registerTreeDataProvider('goTests', goTestsProvider);
 
-    context.subscriptions.push(vscode.commands.registerCommand('gotests.package', (pkgName: string) => {
-        const testCmd = `go test ${pkgName}`;
-        child.exec(testCmd)
+    const outputChannel = vscode.window.createOutputChannel("Go Tests Outline");
+
+    context.subscriptions.push(vscode.commands.registerCommand('gotests.launch', (test: TreeNode) => {
+        test = test || goTestsProvider.selected;
+        let testCmd = '';
+
+        switch (test.type) {
+            case TreeNodeType.package:
+                testCmd = `go test -v ${test.pkgName}`;
+                break;
+            case TreeNodeType.func:
+                testCmd = `go test -v -run '^${test.funcName}$' ${test.pkgName}`;
+                break;
+        }
+
+        child.exec(testCmd, {cwd: vscode.workspace.rootPath}, (e, stdout: string, stderr) => outputChannel.appendLine(stdout))
             .on('exit', code => {
                 if (code != 0) {
                     vscode.window.showErrorMessage('Test(s) failed');
                 } else {
                     vscode.window.showInformationMessage('Test(s) succeded');
                 }
+                outputChannel.show();
             });
     }));
 
-    context.subscriptions.push(vscode.commands.registerCommand('gotests.function', (pkgName: string, funcName: string) => {
-        const testCmd = `go test -run '^${funcName}$' ${pkgName}`;
-        child.exec(testCmd)
+    context.subscriptions.push(vscode.commands.registerCommand('gotests.launch_all', () => {
+        const testCmd = `go test -v ./...`;
+        child.exec(testCmd, {cwd: vscode.workspace.rootPath}, (e, stdout: string, stderr) => outputChannel.appendLine(stdout))
             .on('exit', code => {
                 if (code != 0) {
                     vscode.window.showErrorMessage('Test(s) failed');
                 } else {
                     vscode.window.showInformationMessage('Test(s) succeded');
                 }
+
+                outputChannel.show();
             });
     }));
+
+    vscode.commands.registerCommand('gotests_internal.select', (node: TreeNode) => goTestsProvider.selected = node);
 }
 
 // this method is called when your extension is deactivated
